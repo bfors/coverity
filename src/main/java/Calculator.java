8e2bcc0830595f71257dab84be208436fdec7b74
Brian Forsberg <forsberg@iastate.edu>
2015-02-18 02:50:48 -0600

    Introduced logging framework to project

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,8 +6,18 @@ package main.java;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
     public static void main(String[] args) throws Exception {
+
+        logger.info("Info");
+        logger.debug("Debug");
+        logger.error("Error");
 
         // Create a String stream from first argument
         ANTLRStringStream input = new ANTLRStringStream(args[0]);
@@ -15,9 +25,10 @@ public class Calculator {
         antlr.CalculatorLexer lexer = new antlr.CalculatorLexer(input);
         // Create a stream of tokens fed by the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        // Create a parser (also generated from grammar) that finds meaning in the token stream
+        // Create a parser (also generated from grammar) that draws meaning from the token stream
         antlr.CalculatorParser parser = new antlr.CalculatorParser(tokens);
         // Begin parsing at rule prog, return result
         parser.prog();
     }
+
 }
